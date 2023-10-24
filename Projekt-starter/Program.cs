f461940f5889eba4cb0eb7b881a41e72d3bb013f
Administrator <Administrator@24FIRE>
2023-10-24 19:32:12 +0200

    Projektdateien hinzufügen.

---
+++ Projekt-starter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        string explorerPath = @"C:\FiveM\dev";
+        string vsCodePath = @"C:\FiveM\dev\resources\[dev]\tm";
+
+        ProcessStartInfo explorerStartInfo = new ProcessStartInfo
+        {
+            FileName = "explorer.exe",
+            Arguments = explorerPath,
+            UseShellExecute = true,
+            CreateNoWindow = true
+        };
+        Process.Start(explorerStartInfo);
+
+        ProcessStartInfo vsCodeStartInfo = new ProcessStartInfo
+        {
+            FileName = "code",
+            Arguments = vsCodePath,
+            UseShellExecute = true,
+            CreateNoWindow = true
+        };
+        Process.Start(vsCodeStartInfo);
+
+        // Fügen Sie hier eine Verzögerung hinzu, wenn die Anwendung nach einer bestimmten Zeit automatisch beendet werden soll.
+        // System.Threading.Thread.Sleep(5000); // Warten Sie 5 Sekunden
+    }
+}
